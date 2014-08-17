7991b6ce0b7cb402768455eab1827254e44f65fa
zhalnin <zhalninreg@me.com>
2014-08-17 11:58:10 +0400

    cat 1.4.1 - finish confirm order
    Закончил с подтверждением заказа

--- cart.php
+++ cart.php
@@ -34,22 +34,22 @@ switch ($action) {
 
 if (isset($_POST["submitdata"])) {
 
-    $_POST['order_delivery'] = handleData($_POST['order_delivery']);
-    $_POST['order_fio'] = handleData($_POST['order_fio']);
-    $_POST['order_email'] = handleData($_POST['order_email']);
-    $_POST['order_address'] = handleData($_POST['order_address']);
-    $_POST['order_phone'] = handleData($_POST['order_phone']);
-
-    if ($_POST['order_delivery'] == 'off') {
+    if (isset($_POST['order_delivery']) && $_POST['order_delivery'] != '') {
+        $_POST['order_delivery'] = handleData($_POST['order_delivery']);
+    } else {
         $error[] = 'Выберите один из способов доставки!';
     }
+    $_POST['order_fio']      = handleData($_POST['order_fio']);
+    $_POST['order_email']    = handleData($_POST['order_email']);
+    $_POST['order_address']  = handleData($_POST['order_address']);
+    $_POST['order_phone']    = handleData($_POST['order_phone']);
+
     if (strlen($_POST['order_fio']) < 3 || strlen($_POST['order_fio']) > 50) {
         $error[] = 'Укажите ваши фамилию, имя и отчество от 3 до 50 символов!';
     }
     if (strlen($_POST['order_email']) == "") {
         $error[] = 'Укажите E-mail!';
-    }
-    if (!preg_match('|^[-a-z0-9_\.]+\@[-a-z0-9_\.]+\.[a-z]{2,6}$|i',$_POST['order_email'])) {
+    } else if(!preg_match('|^[-a-z0-9_\.]+\@[-a-z0-9_\.]+\.[a-z]{2,6}$|i',$_POST['order_email'])) {
         $error[] = 'Укажите корректный E-mail!';
     }
     if (strlen($_POST['order_address']) == "") {
@@ -62,11 +62,11 @@ if (isset($_POST["submitdata"])) {
         $_SESSION['order_msg'] = "<p align='left' id='form-error'>".implode('<br />', $error)."</p>";
     } else {
         $_SESSION['order_delivery'] = $_POST['order_delivery'];
-        $_SESSION['order_fio'] = $_POST['order_fio'];
-        $_SESSION['order_email'] = $_POST['order_email'];
-        $_SESSION['order_phone'] = $_POST['order_phone'];
-        $_SESSION['order_address'] = $_POST['order_address'];
-        $_SESSION['order_note'] = $_POST['order_note'];
+        $_SESSION['order_fio']      = $_POST['order_fio'];
+        $_SESSION['order_email']    = $_POST['order_email'];
+        $_SESSION['order_phone']    = $_POST['order_phone'];
+        $_SESSION['order_address']  = $_POST['order_address'];
+        $_SESSION['order_note']     = $_POST['order_note'];
 
         header("Location: cart.php?action=completion");
     }
@@ -77,12 +77,13 @@ $sth1->execute();
 $rows1 = $sth1->fetchAll();
 if (! empty($rows1)) {
     $itogPriceCart = 0;
-    $int = 0;
+    $int1 = 0;
     foreach ($rows1 as $row1) {
-        $int = $int + ($row1["price"] * $row1["cart_count"]);
+        $int1 = $int1 + ($row1["price"] * $row1["cart_count"]);
 //        $int = $row1['price'] * $row1['cart_count'];
+//        echo "<tt><pre>".print_r($row1['price'], true)."</pre></tt>";
     }
-    $itogPriceCart = $int;
+    $itogPriceCart = $int1;
 }
 
 
@@ -221,11 +222,19 @@ try {
                         <p>Шаг 2 из 3</p>
                     </div>';
 
-                    if ($_SESSION['order_delivery'] == "По почте") $chck1 = "checked";
-                    if ($_SESSION['order_delivery'] == "Курьером") $chck2 = "checked";
-                    if ($_SESSION['order_delivery'] == "Самовывоз") $chck3 = "checked";
+
+                    if (isset($_SESSION['order_delivery']) && $_SESSION['order_delivery'] != '') {
+                        $orderDelivery = $_SESSION['order_delivery'];
+                    } else {
+                        $orderDelivery = $_POST['order_delivery'];
+                    }
+                    $chck1 = $chck2 = $chck3 = '';
+                    if ($orderDelivery == "По почте") $chck1 = "checked";
+                    if ($orderDelivery == "Курьером") $chck2 = "checked";
+                    if ($orderDelivery == "Самовывоз") $chck3 = "checked";
 
                     echo '<h3 class="title-h3">Способы доставки: </h3>';
+
                     if (isset($_SESSION["order_msg"]) && $_SESSION["order_msg"]) {
                         echo $_SESSION["order_msg"];
                         unset($_SESSION["order_msg"]);
@@ -233,29 +242,55 @@ try {
             echo '<form method="POST">
                     <ul id="info-radio">
                         <li>
-                            <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery1" value="По почте" '.$chck1.' />
+                            <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery1" value="По почте" ' .$chck1.' />
                             <label class="label_delivery" for="order_delivery1">По почте</label>
                         </li>
                         <li>
-                            <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery2" value="Курьером" '.$chck2.' />
+                            <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery2" value="Курьером"  '.$chck2.' />
                             <label class="label_delivery" for="order_delivery2">Курьером</label>
                         </li>
                         <li>
-                            <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery3" value="Самовывоз" '.$chck3.' />
+                            <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery3" value="Самовывоз" ' .$chck3.' />
                             <label class="label_delivery" for="order_delivery3">Самовывоз</label>
                         </li>
                     </ul>
                     <h3 class="title-h3">Информация для доставки: </h3>
                     <ul id="info-order">';
                     if ($_SESSION['auth'] != 'yes_auth') {
-                        echo '<li><label for="order_fio"><span>*</span>ФИО</label><input type="text" name="order_fio" id="order_fio" value="'.$_SESSION["order_fio"].'" /><span class="order_span_style">Пример: Иванов Иван Иванович</span></li>
-                            <li><label for="order_email"><span>*</span>E-mail</label><input type="text" name="order_email" id="order_email" value="'.$_SESSION["order_email"].'"  /><span class="order_span_style">Пример: ivanov@mail.ru</span></li>
-                            <li><label for="order_phone"><span>*</span>Телефон</label><input type="text" name="order_phone" id="order_phone" value="'.$_SESSION["order_phone"].'"  /><span class="order_span_style">Пример: 8 950 333-22-11</span></li>
-                            <li><label class="order_label_style" for="order_address"><span>*</span>Адрес<br /> доставки</label><input type="text" name="order_address" id="order_address" value="'.$_SESSION["order_address"].'"  /><span class="order_span_style">Пример: г. Москва,<br /> ул. Строителей 35, корп.2, кв.35 </span></li>';
+                        if (isset($_SESSION["order_fio"]) && $_SESSION["order_fio"] != '') {
+                            $orderFio = $_SESSION["order_fio"];
+                        } else {
+                            $orderFio = $_POST['order_fio'];
+                        }
+                        if (isset($_SESSION["order_email"]) && $_SESSION["order_email"] != '') {
+                            $orderEmail = $_SESSION["order_email"];
+                        } else {
+                            $orderEmail =  $_POST['order_email'];
+                        }
+                        if (isset($_SESSION["order_phone"]) && $_SESSION["order_phone"] != '') {
+                            $orderPhone = $_SESSION["order_phone"];
+                        } else {
+                            $orderPhone = $_POST['order_phone'];
+                        }
+                        if (isset($_SESSION["order_address"]) && $_SESSION["order_address"] != '') {
+                            $orderAddress = $_SESSION["order_address"];
+                        } else {
+                            $orderAddress = $_POST['order_address'];
+                        }
+
+                        echo '<li><label for="order_fio"><span>*</span>ФИО</label><input type="text" name="order_fio" id="order_fio" value="'.$orderFio.'" /><span class="order_span_style">Пример: Иванов Иван Иванович</span></li>
+                              <li><label for="order_email"><span>*</span>E-mail</label><input type="text" name="order_email" id="order_email" value="'.$orderEmail.'"  /><span class="order_span_style">Пример: ivanov@mail.ru</span></li>
+                              <li><label for="order_phone"><span>*</span>Телефон</label><input type="text" name="order_phone" id="order_phone" value="'.$orderPhone.'"  /><span class="order_span_style">Пример: 8 950 333-22-11</span></li>
+                              <li><label class="order_label_style" for="order_address"><span>*</span>Адрес<br /> доставки</label><input type="text" name="order_address" id="order_address" value="'.$orderAddress.'"  /><span class="order_span_style">Пример: г. Москва,<br /> ул. Строителей 35, корп.2, кв.35 </span></li>';
+                    }
+                    if (isset($_SESSION["order_note"]) && $_SESSION["order_note"] != '') {
+                        $orderNote = $_SESSION["order_note"];
+                    } else {
+                        $orderNote =  $_POST['order_note'];
                     }
 
                     echo '<li><label class="order_label_style" for="order_note">Примечание</label>
-                            <textarea name="order_note">'.$_SESSION["order_note"].'</textarea><span>Уточните информацию о заказе.<br /> Например, удобное время для звонка<br /> нашего менеджера</span></li></ul>
+                            <textarea name="order_note">'.$orderNote.'</textarea><span>Уточните информацию о заказе.<br /> Например, удобное время для звонка<br /> нашего менеджера</span></li></ul>
                         <p align="right"><input type="submit" name="submitdata" id="confirm-button-next" value="Далее" /></p>
                     </form>';
                     break;
